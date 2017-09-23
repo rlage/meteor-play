8fa5067968faefa00c0c72f988e12bd2f5061e8b
Rafael Lage Tavares <rlt.rafael@gmail.com>
2017-09-23 11:41:12 -0300

    Put name and date on footer. Fixes issue #3

--- imports/ui/components/Footer/Footer.js
+++ imports/ui/components/Footer/Footer.js
@@ -1,19 +1,19 @@
 import React from 'react';
-import { year } from '@cleverbeagle/dates';
+import { year, monthDayYear } from '@cleverbeagle/dates';
 import { Link } from 'react-router-dom';
 import { Grid } from 'react-bootstrap';
 
 import './Footer.scss';
 
 const copyrightYear = () => {
-  const currentYear = year();
-  return currentYear === '2017' ? '2017' : `2017-${currentYear}`;
+  const date = monthDayYear()
+  return date
 };
 
 const Footer = () => (
   <div className="Footer">
     <Grid>
-      <p className="pull-left">&copy; {copyrightYear()} Application Name</p>
+      <p className="pull-left">&copy; {copyrightYear()} Rafael Lage Tavares</p>
       <ul className="pull-right">
         <li><Link to="/terms">Terms<span className="hidden-xs"> of Service</span></Link></li>
         <li><Link to="/privacy">Privacy<span className="hidden-xs"> Policy</span></Link></li>
