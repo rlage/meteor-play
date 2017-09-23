95cb3b731c30abbc6c9df986babd96562acfdec1
Rafael Lage Tavares <rlt.rafael@gmail.com>
2017-09-23 20:07:42 -0300

    Fixes #5

--- imports/ui/pages/Index/Index.js
+++ imports/ui/pages/Index/Index.js
@@ -1,15 +1,44 @@
 import React from 'react';
 import { Button } from 'react-bootstrap';
+import axios from 'axios';
 
 import './Index.scss';
 
-const Index = () => (
-  <div className="Index">
-    <img
-      src="https://media.giphy.com/media/tsgNNs93oIbwk/giphy.gif"
-      alt="Funny gif"
-    />
-  </div>
-);
+class Index extends React.Component  {
+  constructor(props) {
+    super(props);
+    this.state = {
+      data: null,
+    };
+  }
+
+  componentDidMount() {
+    let self = this
+    
+    axios.get('https://api.giphy.com/v1/gifs/random', {
+      params: {
+        api_key: "kLuLZJo74W562Tiz4OkYtT1I3cMM4pWG"
+      }
+    })
+      .then(function (response) {
+        self.setState(response.data)
+      })
+      .catch(function (error) {
+        console.log(error);
+      })
+  }
+  render() {
+    return(
+      <div className="Index">
+        {this.state.data ? 
+        <img
+          src={this.state.data.image_original_url}
+          alt="Funny gif"
+        /> : "Loading..."
+        }
+      </div>
+    )
+  }
+};
 
 export default Index;
